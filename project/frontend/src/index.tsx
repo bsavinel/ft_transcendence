560f578ad68e1aac1ea00271b0a0ec9beb80452b
ranuytte <ranuytte@student.42.fr>
2023-03-06 19:39:06 +0100

    FIX index.tsx --replace class by function

--- project/frontend/src/index.tsx
+++ project/frontend/src/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class App extends React.Component {
-	render () {
+function App() {
 		return (
 			<div className='app'>
                 <h1>Au boulot!</h1>
 			</div>
 		);
-	};
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
