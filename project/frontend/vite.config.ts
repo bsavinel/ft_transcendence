c3338a662687dd738063d6ab054ceb35f35cf551
ranuytte <ranuytte@student.42.fr>
2023-03-14 17:30:08 +0100

    FIX vite --enable require()

--- project/frontend/vite.config.ts
+++ project/frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
 		host: "0.0.0.0",
 		port: 3000,
 	},
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), vitePluginRequire()],
 });
